0d400cd6aa84ec758cbe28d085f9227e06e1f4bf
Primoz <primoz969@gmail.com>
2020-12-12 16:43:16 +0100

    adds signin styles and form, adds collection preview

--- src/App.js
+++ src/App.js
@@ -4,19 +4,17 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE </h1>
-  </div>
-);
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component';
 
 class App extends Component {
   render() {
     return <div className='App'>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/shop/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>;
   }
